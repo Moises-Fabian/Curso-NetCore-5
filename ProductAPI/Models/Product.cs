22e5ac3d55da0aedc1ddcc9e06465ccd0d034522
Moises <m.lagos1995@gmail.com>
2021-02-11 00:20:54 -0300

    se declaro los atributos de la clase Product

--- ProductAPI/Models/Product.cs
+++ ProductAPI/Models/Product.cs
@@ -7,5 +7,18 @@ namespace ProductAPI.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Precio { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public double Rating { get; set; }
     }
 }
