e19f24ebe2a569a891589edd8c50cf86183b018e
Moises <m.lagos1995@gmail.com>
2021-02-12 00:38:39 -0300

    se creo la carpeta y la clase DTO, y se estableció la annotation key y requiere en la clase product

--- ProductAPI/Models/DTO/ProductDTO.cs
+++ ProductAPI/Models/DTO/ProductDTO.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ProductAPI.Models
+namespace ProductAPI.Models.DTO
 {
-    public class Product
+    public class ProductDTO
     {
-        [Key]
         public int Id { get; set; }
-        [Required]
+
         public string Name { get; set; }
 
         public string Description { get; set; }
-        [Required]
+
         public double Precio { get; set; }
 
         public DateTime CreateDate { get; set; }
