84b23928fdfd780673b84437dc51e4ab7a41bda1
Moises <m.lagos1995@gmail.com>
2021-02-12 01:01:30 -0300

    creacion del controlador que contendrá los métodos http

---
+++ ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        readonly IProductRepository productRepository;
+        private readonly IMapper mapper;
+
+        public ProductController(IProductRepository productRepository, IMapper mapper)
+{
+            this.productRepository = productRepository;
+            this.mapper = mapper;
+        }
+    }
+}
