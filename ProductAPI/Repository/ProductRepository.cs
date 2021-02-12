5e56cf0d5533e5e3563b5fc6aad00c5024383517
Moises <m.lagos1995@gmail.com>
2021-02-11 23:43:14 -0300

    Se estalecio la interfaz pública, y se creó la clase ProductRepository

---
+++ ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using ProductAPI.Models;
+using ProductAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public bool CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetallProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ProductoExists(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ProductoExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
