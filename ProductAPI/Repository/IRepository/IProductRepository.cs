1ca1b6d533e6ce4aa4adf8e863db94d7ae82b422
Moises <m.lagos1995@gmail.com>
2021-02-11 23:39:45 -0300

    Se codificó la interfaz IProductRepository

--- ProductAPI/Repository/IRepository/IProductRepository.cs
+++ ProductAPI/Repository/IRepository/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProductAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,20 @@ namespace ProductAPI.Repository.IRepository
 {
     interface IProductRepository
     {
+        IEnumerable<Product> GetallProduct();
+
+        Product GetProduct(int id);
+
+        bool ProductoExists(string name);
+
+        bool ProductoExists(int id);
+
+        bool CreateProduct(Product product);
+
+        bool UpdateProduct(Product product);
+
+        bool DeleteProduct(Product product);
+
+        bool Save();
     }
 }
