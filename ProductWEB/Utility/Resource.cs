f704e4739891ef80ce892ae5d0ca0c4f34b63bf1
Moises <m.lagos1995@gmail.com>
2021-02-13 23:25:12 -0300

    se configuro httpClient en startup y se agrego la carpeta utility con la clase resource, aplicando las propiedades para consumir la api

---
+++ ProductWEB/Utility/Resource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWEB.Utility
+{
+    public class Resource
+    {
+        public const string ApiBaseUrl = "https://localhost:44381";
+        public const string ProductAPIUrl = "api/Product";
+        public const string ContentType = "application/json";
+    }
+}
