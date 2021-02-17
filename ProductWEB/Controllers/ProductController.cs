3692a984d0dffbecd7958c9283b530740af60c2e
Moises <m.lagos1995@gmail.com>
2021-02-16 23:29:02 -0300

    se creo la accion para crear producto

--- ProductWEB/Controllers/ProductController.cs
+++ ProductWEB/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using ProductWEB.Repository.IRepository;
 using ProductWEB.Utility;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -23,6 +24,47 @@ namespace ProductWEB.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await util.GetAllAsync(Resource.ProductAPIUrl));
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Product());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<ActionResult> Create (Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count >0)
+                {
+                    byte[] imgBytes = null;
+                    using (Stream stream = files[0].OpenReadStream())
+                    {
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            await stream.CopyToAsync(memoryStream);
+                            imgBytes = memoryStream.ToArray();
+                        }
+                    }
+                    product.Image = imgBytes;
+                }
+                var modelStateError = await util.CreateAsync(Resource.ProductAPIUrl, product);
+
+                if (modelStateError.Response.Errors.Count > 0)
+                {
+                    foreach (var item in modelStateError.Response.Errors)
+                    {
+                        product.Errors.Add(item);
+                    }
+                    return View(product);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
         }
     }
 }
