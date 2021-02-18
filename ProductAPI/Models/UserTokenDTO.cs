378f81d5b820222a493b23096b8355f55a6ea873
Moises <m.lagos1995@gmail.com>
2021-02-17 23:25:54 -0300

    agregando las clases userDTO y UserTokenDTO

---
+++ ProductAPI/Models/UserTokenDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Models
+{
+    public class UserTokenDTO
+    {
+        public string Token { get; set; }
+
+        public string UserName { get; set; }
+
+        public List<string> Roles { get; set; }
+    }
+}
